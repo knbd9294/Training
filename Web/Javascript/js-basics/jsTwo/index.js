a8347287af534d32cde71b0301dfbeb54864313c
Kemeni Nguendjio <k1n2b3d4@yahoo.fr>
2022-04-26 05:07:48 +0100

    Add web folder

---
+++ Web/Javascript/js-basics/jsTwo/index.js
@@ -0,0 +1,11 @@
+let person = {
+  name: "Mosh",
+  age: 30,
+}; //Object Litteral
+
+person.name = "John";
+person["name"] = "Marry";
+// console.log(person);
+
+let selectedColors = ["red", "blue"]; //Array litteral
+selectedColors[2] = 1;
